bbade922aeda02d4386c5e09871dceb2deeeaf7a
Seth Brown <seth@pop-os.localdomain>
2021-04-22 08:29:28 -0400

    Add Controlled Form

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -3,32 +3,63 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Directory from './DirectoryComponents';
 import CampsiteInfo from './CampsiteInfoComponent';
+import Home from './HomeComponent';
+import Contact from './ContactComponent';
+import About from './AboutComponent';
+import {COMMENTS} from '../shared/comments';
+import {PARTNERS} from '../shared/partners';
+import {PROMOTIONS} from '../shared/promotions';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {CAMPSITES} from '../shared/campsites';
 
 
 class Main extends Component {
+
   constructor(props) {
       super(props);
       this.state = {
-        campsites: CAMPSITES,
-        selectedCampsite: null
+          campsites: CAMPSITES,
+          comments: COMMENTS,
+          partners: PARTNERS,
+          promotions: PROMOTIONS
       };
-    }
-
-    onCampsiteSelect(campsiteId){
-        this.setState({selectedCampsite: campsiteId});
-    }
-
+  }
 
   render() {
-    return(
-      <div>
-        <Header />
-        <Directory campsites={this.state.campsites} onClick={campsiteId => this.onCampsiteSelect(campsiteId)}/>
-        <CampsiteInfo campsite={this.state.campsites.filter(campsite => campsite.id === this.state.selectedCampsite)[0]} />
-        <Footer />
-      </div>
-    );
+
+      const HomePage = () => {
+          return (
+              <Home 
+                campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                partner={this.state.partners.filter(partner => partner.featured)[0]}
+              />
+          );
+      };
+
+      const CampsiteWithId = ({match}) => {
+          return(
+              <CampsiteInfo 
+              campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+              comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+              />
+          );
+      };
+      
+      return (
+          <div>
+              <Header />
+              <Switch>
+                  <Route path='/home' component={HomePage} />
+                  <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                  <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                  <Route path='/aboutus' render={() => <About partners={this.state.partners} />}/>
+                  <Route exact path='/contactus' component={Contact} />
+                  <Redirect to='/home' />
+              </Switch>
+              <Footer />
+          </div>
+      );
   }
 }
 
