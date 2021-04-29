79b68251fe3fd696deb7d243b886230785ee16a9
Seth Brown <seth@pop-os.localdomain>
2021-04-29 12:11:02 -0400

    Add redux form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,6 +1,128 @@
-import React from "react";
-import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import React, {Component} from "react";
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, Button,
+Modal, ModalHeader, ModalBody, Label} from "reactstrap";
+import {Control, LocalForm, Errors} from 'react-redux-form';
 import {Link} from 'react-router-dom';
+import { Loading } from './LoadingComponent';
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const selector = val => val !== "Select"; 
+
+class CommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isModalOpen: false,
+      number: '',
+      author: '',
+      comment: '',
+      touched: {
+        number: false,
+        author: false,
+        comment: false
+      }
+    };
+
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  toggleModal(){
+    this.setState({
+        isModalOpen: !this.state.isModalOpen
+    });
+  }
+  
+  handleSubmit(values){
+    this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
+  }
+
+  handleChange(event){
+    this.setState({value: event.target.value});
+  }
+
+  render(){
+    return(
+      <div>
+        <Button outline onClick={this.toggleModal}>
+          <i className="fa fa-pencil fa-lg" />Submit Comment
+        </Button>
+
+    <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+      <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+      <ModalBody>
+        <LocalForm onSubmit={values => this.handleSubmit(values)}>
+          <div className="form-group">
+            <Label htmlFor="rating">Rating</Label>
+              <Control.select model=".rating" id="rating" name="rating"
+                placeholder="Rating"
+                className="form-control"
+                validators={{
+                  required,
+                  selector
+                }}
+                >
+                <option value="Select">Select</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </Control.select>
+
+              <Errors
+                className="text-danger"
+                model=".rating"
+                show="touched"
+                component="div"
+                messages={{
+                  required: 'Required',
+                  selector: "You must choose a number"
+                }}
+              />
+          </div>
+          <div className="form-group">
+            <Label htmlFor="author">Author</Label>
+              <Control.text model=".author" id="author" name="author"
+                placeholder="Your Name"
+                className="form-control"
+                validators={{
+                  minLength: minLength(2),
+                  maxLength: maxLength(15),
+                  required
+                }}
+              />
+              <Errors
+                className="text-danger"
+                model=".author"
+                show="touched"
+                component="div"
+                messages={{
+                  required: 'Required',
+                  minLength: 'Must be at least 1',
+                  maxLength: 'Must be a 5 or less'
+                }}
+              />
+            </div>
+          <div className="form-group">
+            <Label htmlFor="comment">Comment</Label>
+              <Control.textarea model=".comment" id="comment" name="comment"
+                className="form-control"
+              />
+          </div>
+          <Button type="submit" value="submit" color="primary">Submit</Button>
+        </LocalForm>
+      </ModalBody>
+    </Modal>
+    </div>
+    );
+  }
+}
+
 
 function RenderCampsite({campsite}) {
   return (
@@ -15,12 +137,12 @@ function RenderCampsite({campsite}) {
   );
 }
 
-function RenderComments({comments}) {
+function RenderComments({comments, addComment, campsiteId}) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
         <h4>Comments</h4>
-        {comments.map((comment) => (
+        {comments.map(comment => (
           <div key={comment.id}>
             <p>
               {comment.text}
@@ -34,6 +156,7 @@ function RenderComments({comments}) {
             </p>
           </div>
         ))}
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   } else {
@@ -42,6 +165,26 @@ function RenderComments({comments}) {
 }
 
 function CampsiteInfo(props) {
+  if(props.isLoading){
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  }
+  if(props.errMess){
+    return(
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <h4>{props.errMess}</h4>
+          </div>
+        </div>
+      </div>
+    );
+  }
   if (props.campsite) {
     return (
       <div className="container">
@@ -57,7 +200,11 @@ function CampsiteInfo(props) {
           </div>
           <div className="row">
               <RenderCampsite campsite={props.campsite} />
-              <RenderComments comments={props.comments} />
+              <RenderComments 
+                comments={props.comments}
+                addComment={props.addComment}
+                campsiteId={props.campsite.id}
+              />
           </div>
       </div>
   );
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,8 @@ import Contact from './ContactComponent';
 import About from './AboutComponent';
 import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 import {connect} from 'react-redux';
+import {actions} from 'react-redux-form';
+import {addComment, fetchCampsites} from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return{
@@ -18,15 +20,26 @@ const mapStateToProps = state => {
     }
 }
 
+const mapDispatchToProps = {
+    addComment: (campsiteId, rating, author, text) => (addComment(campsiteId, rating, author, text)),
+    fetchCampsites: () => (fetchCampsites()),
+    resetFeedbackForm: () => (actions.reset('feedbackForm'))
+};
+
 class Main extends Component {
 
+  componentDidMount(){
+    this.props.fetchCampsites();
+  }
 
   render() {
 
       const HomePage = () => {
           return (
               <Home 
-                campsite={this.props.campsites.filter(campsite => campsite.featured)[0]}
+                campsite={this.props.campsites.campsites.filter(campsite => campsite.featured)[0]}
+                campsitesLoading={this.props.campsites.isLoading}
+                campsitesErrMess={this.props.campsites.errMess}
                 promotion={this.props.promotions.filter(promotion => promotion.featured)[0]}
                 partner={this.props.partners.filter(partner => partner.featured)[0]}
               />
@@ -36,8 +49,11 @@ class Main extends Component {
       const CampsiteWithId = ({match}) => {
           return(
               <CampsiteInfo 
-              campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+              campsite={this.props.campsites.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+              isLoading={this.props.campsites.isLoading}
+              errMess={this.props.campsites.errMess}
               comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+              addComment={this.props.addComment}
               />
           );
       };
@@ -50,7 +66,7 @@ class Main extends Component {
                   <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
                   <Route path='/directory/:campsiteId' component={CampsiteWithId} />
                   <Route path='/aboutus' render={() => <About partners={this.props.partners} />}/>
-                  <Route exact path='/contactus' component={Contact} />
+                  <Route exact path='/contactus' render={() => <Contact resetFeedbackForm={this.props.resetFeedbackForm} />} />
                   <Redirect to='/home' />
               </Switch>
               <Footer />
@@ -59,4 +75,4 @@ class Main extends Component {
   }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
